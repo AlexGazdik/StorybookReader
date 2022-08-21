80a49459e87adfe650bc1d25c319f0308beaa354
alex_is <45057376+AlexGazdik@users.noreply.github.com>
2022-08-21 11:00:58 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let typed6 = new Typed('#typed6', {
+    strings: ["When I was little, my father was famous. He was the greatest Samurai in the empire, and he was the Shogun's decapitator.^2000\n He cut off the heads of 131 lords for the Shogun. It was a bad time for the empire."],
+    typeSpeed: 60,
+    backSpeed: false,
+    loop: false,
+    showCursor: false
+  });
