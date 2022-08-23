fd959612af3a6911e2843a45535a2b361e20d9b9
alex_is <45057376+AlexGazdik@users.noreply.github.com>
2022-08-22 22:34:47 -0400

    update

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+let next = document.querySelector(".next")
+let start = document.querySelector(".start")
+let stop = document.querySelector(".stop")
+let reset = document.querySelector(".reset")
 
+next.addEventListener('click', () => console.log('Hello') )
+
+// const creatStop = function(){
+//   typed6.stop()
+// }
 
 let typed6 = new Typed('#typed6', {
-    strings: ["When I was little, my father was famous. He was the greatest Samurai in the empire, and he was the Shogun's decapitator.^2000\n He cut off the heads of 131 lords for the Shogun. It was a bad time for the empire."],
-    typeSpeed: 60,
+    strings: ["When i was little, my father was famous. He was the greatest samurai in the empire, and he was the shoguns decapitator. He cut off the heads of a hundred and thirty one lords. It was a bad time for the empire. The shogun justn stayed inside his castle and he never came out. People said his brain was infected by devils"],
+    typeSpeed: 100,
     backSpeed: false,
     loop: false,
-    showCursor: false
+    cursorChar: '...<strong class="blue">Ⓐ</strong>',
+    autoInsertCss: true,
   });
+
+
+  //make start stop reset and speed control buttons. thats it .push to main!!! and move!!!
